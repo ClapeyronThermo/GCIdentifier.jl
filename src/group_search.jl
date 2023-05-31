15c927c5540f5e884b5380ac40cf1ccf9b3d6dd2
longemen3000 <longemen3000@gmail.com>
2023-05-31 04:14:00 -0400

    flatten the loop structure in group_search

--- src/group_search.jl
+++ src/group_search.jl
@@ -74,12 +74,13 @@ function get_groups_from_smiles(smiles::String,groups::Vector{GCPair};connectivi
 
     #step 0.b find all overlaps within the matched groups.
 
-
     for (idx,smatch) in pairs(smatches)
         i = smatches_idx[idx]
         group_i = possible_groups[idx]
         smarts_i = smarts(group_i)
         for j in 1:length(smatch)
+            
+            #add first match.
             if isempty(atoms_list)
                 push!(group_list,smarts_i)
                 push!(group_id,i)
@@ -87,72 +88,77 @@ function get_groups_from_smiles(smiles::String,groups::Vector{GCPair};connectivi
                 append!(atoms_list,smatch[j]["atoms"])
                 append!(coverage_atoms,[smatch[j]["atoms"]])
                 append!(coverage_bonds,[smatch[j]["bonds"]])
-            else
-                # If no atoms covered by this group are already covered by other groups
-                if sum(smatch[j]["atoms"] .∈ [atoms_list])==0 
-                    append!(atoms_list,smatch[j]["atoms"])
-                    if !(smarts_i in group_list)
-                        push!(group_list,smarts_i)
-                        push!(group_id,i)
-                        push!(group_occ_list,1)
-                        append!(coverage_atoms,[smatch[j]["atoms"]])
-                        append!(coverage_bonds,[smatch[j]["bonds"]])
-                    else
-                        group_occ_list[end] += 1
-                        append!(coverage_atoms[end],smatch[j]["atoms"])
-                        append!(coverage_bonds[end],smatch[j]["bonds"])
-                    end
-                else 
-                    # Check which groups group i has an overlap with
-                    id = 0
-                    ng_rm = 0
-                    for k in 1:length(group_id)
-                        id += 1
-                        # Does group 1 cover any atoms of group id
-                        if sum(smatch[j]["atoms"] .∈ [coverage_atoms[id]])>0
-                            # We only care if group i covers _more_ atoms than group k
-                            if ((length(smatch[j]["atoms"])>length(coverage_atoms[id])) & 
-                                # Also make sure that group i covers all the atoms of group k 
-                                (sum(smatch[j]["atoms"] .∈ [coverage_atoms[id]]).==length(coverage_atoms[id]))) |
-                                (length(smatch[j]["bonds"])>length(coverage_bonds[id]))
-                                # find out which atoms are covered
-                                overlap_atoms = coverage_atoms[id][coverage_atoms[id] .∈ [smatch[j]["atoms"]]]
-                                id_rm = group_id[id]
-                                name_rm = group_list[id]
-                                bond_rm =  coverage_bonds[id][coverage_bonds[id] .∈ [smatch[j]["bonds"]]]
-                                filter!(e->e ∉ overlap_atoms,atoms_list)
-                                filter!(e->e ∉ overlap_atoms,coverage_atoms[id])
-                                group_occ_list[id] -= 1
+                continue #go to next iteration
+            end
 
-                                # If group k no longer covers any atoms, remove it
-                                if group_occ_list[id] == 0
-                                    filter!(e->e ≠ 0,group_occ_list)
-                                    filter!(e->!isempty(e),coverage_atoms)
-                                    deleteat!(coverage_bonds,id)
-                                    filter!(e->e≠id_rm,group_id)
-                                    filter!(e->e≠name_rm,group_list)
-                                    id -= 1 
-                                end
-                                ng_rm +=1
-                            end
-                        end    
-                    end
-                    if ng_rm > 0
-                        if !(smarts_i in group_list)
-                            push!(group_list,smarts_i)
-                            push!(group_id,i)
-                            push!(group_occ_list,1)
-                            append!(coverage_atoms,[smatch[j]["atoms"]])
-                            append!(coverage_bonds,[smatch[j]["bonds"]])
-                            append!(atoms_list,smatch[j]["atoms"])
-                        else
-                            group_occ_list[end] += 1
-                            append!(atoms_list,smatch[j]["atoms"])
-                            append!(coverage_atoms[end],smatch[j]["atoms"])
-                            append!(coverage_bonds[end],smatch[j]["bonds"])
-                        end
-                    end
+            # If no atoms covered by this group are already covered by other groups            
+            if sum(smatch[j]["atoms"] .∈ [atoms_list]) == 0 
+                append!(atoms_list,smatch[j]["atoms"])
+                if !(smarts_i in group_list)
+                    push!(group_list,smarts_i)
+                    push!(group_id,i)
+                    push!(group_occ_list,1)
+                    append!(coverage_atoms,[smatch[j]["atoms"]])
+                    append!(coverage_bonds,[smatch[j]["bonds"]])
+                    append!(atoms_list,smatch[j]["atoms"])
+                else
+                    group_occ_list[end] += 1
+                    append!(coverage_atoms[end],smatch[j]["atoms"])
+                    append!(coverage_bonds[end],smatch[j]["bonds"])
                 end
+                continue
+            end
+            
+            # Check which groups group i has an overlap with
+            id = 0
+            ng_rm = 0
+            for k in 1:length(group_id)
+                id += 1
+
+                # Does group 1 cover any atoms of group id
+                sum(smatch[j]["atoms"] .∈ [coverage_atoms[id]]) <= 0 && continue
+
+                # We only care if group i covers _more_ atoms than group k
+                if ((length(smatch[j]["atoms"])>length(coverage_atoms[id])) & 
+                    # Also make sure that group i covers all the atoms of group k 
+                    (sum(smatch[j]["atoms"] .∈ [coverage_atoms[id]]).==length(coverage_atoms[id]))) |
+                    (length(smatch[j]["bonds"])>length(coverage_bonds[id]))
+                    # find out which atoms are covered
+                    overlap_atoms = coverage_atoms[id][coverage_atoms[id] .∈ [smatch[j]["atoms"]]]
+                    id_rm = group_id[id]
+                    name_rm = group_list[id]
+                    bond_rm =  coverage_bonds[id][coverage_bonds[id] .∈ [smatch[j]["bonds"]]]
+                    filter!(e->e ∉ overlap_atoms,atoms_list)
+                    filter!(e->e ∉ overlap_atoms,coverage_atoms[id])
+                    group_occ_list[id] -= 1
+
+                    # If group k no longer covers any atoms, remove it
+                    if group_occ_list[id] == 0
+                        filter!(e->e ≠ 0,group_occ_list)
+                        filter!(e->!isempty(e),coverage_atoms)
+                        deleteat!(coverage_bonds,id)
+                        filter!(e->e≠id_rm,group_id)
+                        filter!(e->e≠name_rm,group_list)
+                        id -= 1 
+                    end
+                    ng_rm +=1
+                end     
+            end
+
+            ng_rm == 0 && continue
+
+            if !(smarts_i in group_list)
+                push!(group_list,smarts_i)
+                push!(group_id,i)
+                push!(group_occ_list,1)
+                append!(coverage_atoms,[smatch[j]["atoms"]])
+                append!(coverage_bonds,[smatch[j]["bonds"]])
+                append!(atoms_list,smatch[j]["atoms"])
+            else
+                group_occ_list[end] += 1
+                append!(atoms_list,smatch[j]["atoms"])
+                append!(coverage_atoms[end],smatch[j]["atoms"])
+                append!(coverage_bonds[end],smatch[j]["bonds"])
             end
         end
     end
@@ -162,6 +168,7 @@ function get_groups_from_smiles(smiles::String,groups::Vector{GCPair};connectivi
     #end
 
     gcpairs = [name(groups[group_id[i]]) => group_occ_list[i] for i in 1:length(group_id)]
+    
     if connectivity
         return (smiles,gcpairs,get_connectivity(mol,group_id,groups))
     else
