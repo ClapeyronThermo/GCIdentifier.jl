bad73fbf2d0ff767c3df667f3cc111efa744d940
pw0908 <pjwalker@caltech.edu>
2023-10-23 15:43:40 -0700

    Update group names

--- src/database/gcPPCSAFT.jl
+++ src/database/gcPPCSAFT.jl
@@ -1,4 +1,4 @@
-gcPCSAFTgroups = [raw"[CX4H3]" "CH3";  # Already exist
+gcPPCSAFTgroups = [raw"[CX4H3]" "CH3";  # Already exist
 raw"[!R;CX4H2]" "CH2"; # Already exist
 raw"[!R;CX4H]" "CH"; # Already exist
 raw"[!R;CX4H0]" "C"; # Already exist
@@ -13,5 +13,11 @@ raw"[CH1;R1;$(C1AAAAA1)]" "cCH_hex"; # Already exist
 raw"[cX3;H1]" "aCH"; # Already exist
 raw"[cX3;H0]" "aC"; # Already exist
 raw"[OX2H;!$([OX2H]-[#6]=[O]);!$([OX2H]-a)]" "OH"; # Already exist
-raw"[NX3H2]" "NH2"] # Already exist
-export gcPCSAFTgroups
+raw"[NX3H2]" "NH2"; # Already exist
+raw"[$([CX3H0](=[OX1]));!$([CX3](=[OX1])-[OX2]);!R]=O" "C=O";
+raw"[CH;D2](=O)" "CH=O";
+raw"[#6X3H0;!$([#6X3H0](~O)(~O)(~O))](=[#8X1])[#8X2H0]" "COO";
+raw"[CX4H3][OX2H0;!R;!$([OX2H0]-[#6]=[#8])]" "OCH3";
+raw"[CX4H2][OX2H0;!R;!$([OX2H0]-[#6]=[#8])]" "OCH2";
+raw"[CH;D2](=O)[OX2H0;!R;!$([OX2H0]-[#6]=[#8])]" "HCOO"]
+export gcPPCSAFTgroups
