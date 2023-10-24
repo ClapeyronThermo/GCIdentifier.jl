14f8150291352652f8ed37428f9683efb27b2a3e
pw0908 <pjwalker@caltech.edu>
2023-10-24 16:30:06 -0700

    Convert to GCPair

--- src/database/gcPPCSAFT.jl
+++ src/database/gcPPCSAFT.jl
@@ -1,23 +1,23 @@
-gcPPCSAFTGroups = [raw"[CX4H3]" "CH3";  # Already exist
-raw"[!R;CX4H2]" "CH2"; # Already exist
-raw"[!R;CX4H]" "CH"; # Already exist
-raw"[!R;CX4H0]" "C"; # Already exist
-raw"[CX3H2]" "CH2="; # Already exist
-raw"[!R;CX3H1;!$([CX3H1](=O))]" "CH="; # Already exist
-raw"[$([!R;#6X3H0]);!$([!R;#6X3H0]=[#8])]" "=C<"; # Already exist
-raw"[CX2;H1]#[CX2;H0]" "C#CH"; # Already exist
-raw"[CH2;R1;$(C1AAAA1)]" "cCH2_pen"; # Already exist
-raw"[CH1;R1;$(C1AAAA1)]" "cCH_pen"; # Already exist
-raw"[CH2;R1;$(C1AAAAA1)]" "cCH2_hex"; # Already exist
-raw"[CH1;R1;$(C1AAAAA1)]" "cCH_hex"; # Already exist
-raw"[cX3;H1]" "aCH"; # Already exist
-raw"[cX3;H0]" "aC"; # Already exist
-raw"[OX2H;!$([OX2H]-[#6]=[O]);!$([OX2H]-a)]" "OH"; # Already exist
-raw"[NX3H2]" "NH2"; # Already exist
-raw"[$([CX3H0](=[OX1]));!$([CX3](=[OX1])-[OX2]);!R]=O" "C=O";
-raw"[CH;D2](=O)" "CH=O";
-raw"[#6X3H0;!$([#6X3H0](~O)(~O)(~O))](=[#8X1])[#8X2H0]" "COO";
-raw"[CX4H3][OX2H0;!R;!$([OX2H0]-[#6]=[#8])]" "OCH3";
-raw"[CX4H2][OX2H0;!R;!$([OX2H0]-[#6]=[#8])]" "OCH2";
-raw"[CH;D2](=O)[OX2H0;!R;!$([OX2H0]-[#6]=[#8])]" "HCOO"]
+gcPPCSAFTGroups = [GCPair(raw"[CX4H3]","CH3"),
+GCPair(raw"[!R;CX4H2]","CH2"),
+GCPair(raw"[!R;CX4H]","CH"),
+GCPair(raw"[!R;CX4H0]","C"),
+GCPair(raw"[CX3H2]","CH2="),
+GCPair(raw"[!R;CX3H1;!$([CX3H1](=O))]","CH="),
+GCPair(raw"[$([!R;#6X3H0]);!$([!R;#6X3H0]=[#8])]","=C<"),
+GCPair(raw"[CX2;H1]#[CX2;H0]","C#CH"),
+GCPair(raw"[CH2;R1;$(C1AAAA1)]","cCH2_pen"),
+GCPair(raw"[CH1;R1;$(C1AAAA1)]","cCH_pen"),
+GCPair(raw"[CH2;R1;$(C1AAAAA1)]","cCH2_hex"),
+GCPair(raw"[CH1;R1;$(C1AAAAA1)]","cCH_hex"),
+GCPair(raw"[cX3;H1]","aCH"),
+GCPair(raw"[cX3;H0]","aC"),
+GCPair(raw"[OX2H;!$([OX2H]-[#6]=[O]);!$([OX2H]-a)]","OH"),
+GCPair(raw"[NX3H2]","NH2"),
+GCPair(raw"[$([CX3H0](=[OX1]));!$([CX3](=[OX1])-[OX2]);!R]=O","C=O"),
+GCPair(raw"[CH;D2](=O)","CH=O"),
+GCPair(raw"[#6X3H0;!$([#6X3H0](~O)(~O)(~O))](=[#8X1])[#8X2H0]","COO"),
+GCPair(raw"[CX4H3][OX2H0;!R;!$([OX2H0]-[#6]=[#8])]","OCH3"),
+GCPair(raw"[CX4H2][OX2H0;!R;!$([OX2H0]-[#6]=[#8])]","OCH2"),
+GCPair(raw"[CH;D2](=O)[OX2H0;!R;!$([OX2H0]-[#6]=[#8])]","HCOO")]
 export gcPPCSAFTgroups
