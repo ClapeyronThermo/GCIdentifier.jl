64374782028d41b3b96fd4d3a515fb3542939b0d
longemen3000 <longemen3000@gmail.com>
2024-02-15 01:09:29 -0300

    another typo in make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,18 +1,16 @@
 using Documenter,GCIdentifier
 
 makedocs(sitename = "GCIdentifier.jl",
-format = Documenter.HTML(
-    # Use clean URLs, unless built as a "local" build
-    canonical = "https://ClapeyronThermo.github.io/GCIdentifier.jl/",
-),
-warnonly = Documenter.except(),
+    format = Documenter.HTML(
+        # Use clean URLs, unless built as a "local" build
+        canonical = "https://ClapeyronThermo.github.io/GCIdentifier.jl/",
+        ),
+    warnonly = Documenter.except(),
     authors = "Pierre J. Walker and Andrés Riedemann.",
-    pages = [
-        "Home" => "index.md",
-        "Background" => "background.md",
-        "Basic Usage" => "basic_usage.md",        
-        "API" => "api.md"]
+    pages = ["Home" => "index.md",
+            "Background" => "background.md",
+            "Basic Usage" => "basic_usage.md",        
+            "API" => "api.md"]
+    )
 
-        deploydocs(;
-    repo="github.com/ClapeyronThermo/GCIdentifier.jl.git",
-)
+deploydocs(repo="github.com/ClapeyronThermo/GCIdentifier.jl.git")
