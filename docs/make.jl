72fe6bd4b8ffc359161173e1a2a57761f8a24479
longemen3000 <longemen3000@gmail.com>
2024-02-15 00:58:00 -0300

    start docs

---
+++ docs/make.jl
@@ -0,0 +1,18 @@
+using Documenter,GCIdentifier
+
+makedocs(sitename = "GCIdentifier.jl",
+format = Documenter.HTML(
+    # Use clean URLs, unless built as a "local" build
+    canonical = "https://ClapeyronThermo.github.io/GCIdentifier.jl/",
+),
+warnonly = Documenter.except(),
+    authors = "Pierre J. Walker and Andrés Riedemann.",
+    pages = [
+        "Home" => "index.md",
+        "Background" => "background.md",
+        "Basic Usage" => "basic_usage.md",        
+        "API" => "api.md")
+
+        deploydocs(;
+    repo="github.com/ClapeyronThermo/GCIdentifier.jl.git",
+)
