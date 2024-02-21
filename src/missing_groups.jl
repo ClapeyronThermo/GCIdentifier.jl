1209b40679af48d64e8da8e5bc617c7b52cbfacf
pw0908 <pjwalker@caltech.edu>
2024-02-20 18:27:12 -0800

    Fix reduction code

--- src/missing_groups.jl
+++ src/missing_groups.jl
@@ -27,10 +27,17 @@ julia> find_missing_groups_from_smiles("CC(=O)O")
  GCIdentifier.GCPair("[CX3;H0;!R](=[OX1;H0;!R])([OX2;H1;!R])", "C=O=OH")
 ```
 """
-function find_missing_groups_from_smiles(smiles, groups=[], lib=MolecularGraphJL(); max_group_size=nothing, environment=false, reduced=false)
+function find_missing_groups_from_smiles(smiles, groups=nothing, lib=MolecularGraphJL(); max_group_size=nothing, environment=false, reduced=false)
     mol = get_mol(lib, smiles)
-
     __bonds = __getbondlist(lib,mol)
+    atoms = get_atoms(lib,mol)
+
+    if isnothing(groups)
+        missing_atoms = ones(Bool, length(atoms))
+    else
+        smatches_idx_expanded, atom_coverage = find_covered_atoms(mol, groups, lib, atoms, __bonds, false)
+        missing_atoms = (sum(atom_coverage, dims=1) .== 0)[:]
+    end
 
     atom_type = string.(MolecularGraph.atom_symbol(mol))
     graph = MolecularGraph.to_dict(mol)["graph"]
@@ -39,16 +46,18 @@ function find_missing_groups_from_smiles(smiles, groups=[], lib=MolecularGraphJL
         bond_mat[graph[i][1], graph[i][2]] = MolecularGraph.props(mol, graph[i][1], graph[i][2]).order
         bond_mat[graph[i][2], graph[i][1]] = MolecularGraph.props(mol, graph[i][1], graph[i][2]).order
     end
+    atom_type = atom_type[missing_atoms]
+    bond_mat = bond_mat[missing_atoms, missing_atoms]
 
     is_bonded = bond_mat .> 0
 
-    ring = MolecularGraph.is_in_ring(mol)
+    ring = MolecularGraph.is_in_ring(mol)[missing_atoms]
     ring_string = [if i "" else "!" end for i in ring]
 
-    hydrogens = MolecularGraph.total_hydrogens(mol)
-    bonds = MolecularGraph.connectivity(mol)
-    aromatic = MolecularGraph.is_aromatic(mol)
-    hybrid = MolecularGraph.hybridization(mol)
+    hydrogens = MolecularGraph.total_hydrogens(mol)[missing_atoms]
+    bonds = MolecularGraph.connectivity(mol)[missing_atoms]
+    aromatic = MolecularGraph.is_aromatic(mol)[missing_atoms]
+    hybrid = MolecularGraph.hybridization(mol)[missing_atoms]
     atom_type = [if aromatic[i] lowercase(atom_type[i]) else atom_type[i] end for i in 1:length(atom_type)]
 
     natoms = length(atom_type)
@@ -84,72 +93,105 @@ function find_missing_groups_from_smiles(smiles, groups=[], lib=MolecularGraphJL
         return new_groups
     end
 
+    occurrence = ones(Int, length(smarts))
+
     for i in 1:natoms
-        for j in i:natoms
-            if is_bonded[i,j]
-                # If carbon atom on ring
-                if (atom_type[i] == "C" || atom_type[i] == "c") && ring[i]
+        # println(occurrence)
+        # println(smarts[i])
+        # If carbon atom on ring
+        if (atom_type[i] == "C" || atom_type[i] == "c") && ring[i] && occurrence[i] > 0
+            for j in 1:natoms
+                if is_bonded[i,j]
                     # Bond it to any other atom that is non-carbon atom on a ring or carbon atom not on a ring
                     if (atom_type[j] == "C" && !ring[j] || (atom_type[j] != "C" && atom_type[j] != "c"))
                         push!(smarts, smarts[i]*smarts[j])
                         push!(names, names[i]*names[j])
+                        occurrence[i] -= 1
+                        occurrence[j] -= 1
+                        append!(occurrence, [1])
                     end
-                elseif (atom_type[i] != "C" && atom_type[i] != "c") || (atom_type[i] == "C" && !ring[i])
-                    # Bond it to any other atom that is not a carbon atom
-                    nbonds = sum(is_bonded[i,:])
-                    bondable_smarts = smarts[1:natoms][is_bonded[i,:]]
-                    bondable_names = names[1:natoms][is_bonded[i,:]]
-                    bondable_atom_types = atom_type[1:natoms][is_bonded[i,:]]
-                    bond_orders = bond_mat[i, is_bonded[i,:]]
+                end
+            end
+        elseif ((atom_type[i] != "C" && atom_type[i] != "c") || (atom_type[i] == "C" && !ring[i])) && occurrence[i] > 0
+            # Bond it to any other atom that is not a carbon atom
+            nbonds = sum(is_bonded[i,:].&occurrence[1:natoms] .> 0)
+            bondable_atom_types = atom_type[1:natoms][is_bonded[i,:].&occurrence[1:natoms] .> 0]
 
-                    if isnothing(max_group_size)
-                        max_group_size = nbonds+2
-                    end
+            is_carbon = bondable_atom_types .== "C" .|| bondable_atom_types .== "c"
+            ncarbons = sum(is_carbon)
+            nbonds = nbonds - ncarbons
 
-                    if reduced
-                        min_group_size = max_group_size-1
-                    else  
-                        min_group_size = 1
-                    end
-                    for k in min_group_size:max_group_size-1
-                        combs = Combinatorics.combinations(1:nbonds, k)
-                        for comb in combs
-                            if any(bondable_atom_types[comb] .== "C" .|| bondable_atom_types[comb] .== "c")
-                                continue
+            bondable_smarts = smarts[1:natoms][is_bonded[i,:].&occurrence[1:natoms] .> 0][.!(is_carbon)]
+            bondable_names = names[1:natoms][is_bonded[i,:].&occurrence[1:natoms] .> 0][.!(is_carbon)]
+            bondable_atom_types = bondable_atom_types[.!(is_carbon)]
+            bond_orders = bond_mat[i, is_bonded[i,:].&occurrence[1:natoms] .> 0][.!(is_carbon)]
+
+            if isnothing(max_group_size)
+                max_size = nbonds+1
+            else
+                max_size = max_group_size
+            end
+
+            if reduced
+                min_size = max_size-1
+            else  
+                min_size = 1
+            end
+            println(smarts[i])
+
+            println(max_size-1)
+            println(min_size)
+            println(nbonds)
+
+            for k in min_size:max_size-1
+                # println(k)
+                combs = Combinatorics.combinations(1:nbonds, k)
+                for comb in combs
+                    # println(bondable_atom_types[comb])
+                    if any(bondable_atom_types[comb] .== "C" .|| bondable_atom_types[comb] .== "c")
+                        # println(comb)
+                        continue
+                    else
+                        new_smarts = deepcopy(smarts[i])
+                        new_names = deepcopy(names[i])
+                        for l in 1:length(comb)
+                            if bond_orders[comb[l]] == 2
+                                new_smarts *= "(="*bondable_smarts[comb[l]]*")"
+                            elseif bond_orders[comb[l]] == 3
+                                new_smarts *= "(#"*bondable_smarts[comb[l]]*")"
                             else
-                                new_smarts = deepcopy(smarts[i])
-                                new_names = deepcopy(names[i])
-                                for l in 1:length(comb)
-                                    if bond_orders[comb[l]] == 2
-                                        new_smarts *= "(="*bondable_smarts[comb[l]]*")"
-                                    elseif bond_orders[comb[l]] == 3
-                                        new_smarts *= "(#"*bondable_smarts[comb[l]]*")"
-                                    else
-                                        new_smarts *= "("*bondable_smarts[comb[l]]*")"
-                                    end
-                                    new_names *= bondable_names[comb[l]]
-                                end
-                                push!(smarts, new_smarts)
-                                push!(names, new_names)
+                                new_smarts *= "("*bondable_smarts[comb[l]]*")"
                             end
+                            new_names *= bondable_names[comb[l]]
                         end
+                        push!(smarts, new_smarts)
+                        push!(names, new_names)
+                        occurrence[i] -= 1
+                        idx = 1:natoms
+                        idx = idx[is_bonded[i,:].&occurrence[1:natoms] .> 0][.!(is_carbon)][comb]
+                        occurrence[idx] .-= 1
+                        append!(occurrence, [1])
                     end
                 end
             end
         end
     end
 
-    unique_smarts = unique(smarts)
+    if reduced 
+        unique_smarts = unique(smarts[occurrence .> 0])
+    else
+        unique_smarts = unique(smarts)
+    end
     # find the names of the unique smarts
     unique_names = []
-    occurence = zeros(Int, length(unique_smarts))
+    occurrence = zeros(Int, length(unique_smarts))
     for i in 1:length(unique_smarts)
         push!(unique_names, names[findall(x->x==unique_smarts[i], smarts)[1]])
         query_i = get_qmol(lib,unique_smarts[i])
 
-        occurence[i] = length(get_substruct_matches(lib,mol,query_i,__bonds))
+        occurrence[i] = length(get_substruct_matches(lib,mol,query_i,__bonds))
 
-        # println(unique_smarts[i], " ", unique_names[i], " ", occurence[i])
+        # println(unique_smarts[i], " ", unique_names[i], " ", occurrence[i])
     end
 
     new_groups = [GCPair(unique_smarts[i], unique_names[i]) for i in 1:length(unique_smarts)]
