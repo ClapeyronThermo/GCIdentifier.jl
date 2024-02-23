3aae29c1531510bba5cc747d51da14ddab090422
pw0908 <pjwalker@caltech.edu>
2024-02-22 19:33:03 -0800

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,4 +5,6 @@ using GCIdentifier: @gcstring_str
 @testset "All tests" begin
     include("test_group_search.jl")
     include("test_missing_groups.jl")
+    include("test_ext_clapeyron.jl")
+    include("test_ext_chemicalidentifiers.jl")
 end
